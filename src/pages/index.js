2fe573275d5017ac5e09c502f86c370038960bfc
Bartosz Walicki <walicki.bartosz@gmail.com>
2020-08-02 21:35:10 +0200

    Initial steps.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (<div><ul><li><Link to="/about/">About</Link></li></ul>
+    <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div></div>)
 }
