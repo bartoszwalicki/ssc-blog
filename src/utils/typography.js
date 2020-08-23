7eae38aa5eb269c45f20a573ca3cb6726db4ccc0
Bartosz Walicki <walicki.bartosz@gmail.com>
2020-08-23 20:45:24 +0200

    HTML is not overflowed now

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,16 @@
 import Typography from "typography"
 
 const typography = new Typography({
-    baseFontSize: '18px',
+    baseFontSize: '14px',
     baseLineHeight: 1.45,
     headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
     bodyFontFamily: ['Georgia', 'serif'],
-    bodyColor: '#fff'
+    bodyColor: '#fff',
+    overrideStyles: () => ({
+        html: {
+            overflowY: 'initial',
+        },
+    }),
 })
 
 export const { scale, rhythm, options } = typography
